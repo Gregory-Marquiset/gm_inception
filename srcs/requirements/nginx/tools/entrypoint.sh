01ffc66110f819f81c3ef43d9cba8e3d0ba8aad1
gmarquis <gmarquis@VMinception.gmarquis.virtualbox.org>
2025-10-07 12:41:40 +0200

    testing nginx 2

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -15,3 +15,12 @@ if [ ! -f "$SSL_DIR/server.crt" ] || [ ! -f "$SSL_DIR/server.key" ]; then
 fi
 
 exec "$@"
+
+# Testing cmd
+
+#docker compose exec nginx sh -lc 'ls -l /etc/nginx/ssl'
+
+# docker compose exec nginx sh -lc \
+#"openssl x509 -in /etc/nginx/ssl/server.crt -noout -subject -issuer -dates -ext subjectAltName"
+
+#docker compose exec nginx sh -lc 'rm -f /etc/nginx/ssl/server.crt /etc/nginx/ssl/server.key'
